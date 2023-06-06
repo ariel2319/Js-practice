d0ff1582f0df1b3a743039587423465f3e5695cb
areil <maldonadoaa.91@gmail.com>
2023-06-06 01:33:51 -0300

    basic algorithm scripting 2

--- Algoritmos Básicos/app.js
+++ Algoritmos Básicos/app.js
@@ -95,13 +95,79 @@ confirmEnding("Bastian", "nasd");
 //Repite una cadena dada str (primer argumento) por un número (num) de veces (segundo argumento). Devuelve una cadena vacía si num no es un número positivo. Para este desafío, no utilices el método incorporado .repeat().
 cls();
 function repeatStringNumTimes(str, num) {
-  let aux = []
+  let aux = "";
+
   if (num < 0) return aux;
 
-  for (let i=0; i<num; i++){
-    str = 
+  for (let i = 0; i < num; i++) {
+    aux += str;
+    console.log(aux)
   }
-
+  return aux
 }
 
 repeatStringNumTimes("abc", 3);
+
+//todo Recorta una cadena
+//Recorta una cadena (primer argumento) si es más larga que la longitud máxima proporcionada (segundo argumento). Devuelve la cadena recortada con un ... al final.
+
+cls();
+function truncateString(str, num) {
+  let aux;
+  if (str.length > num) {
+    aux = str.slice(0, num) + "..."
+    //console.log(aux)
+    return aux;
+  }
+
+  return aux;
+}
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length)
+//truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+//todo Buscadores Guardianes
+//Crea una función que recorra un arreglo arr y devuelva el primer elemento que pase un "detector de verdad". Esto significa que dado un elemento x, el "detector de verdad" es pasado si func(x) es true. Si ningún elemento pasa la prueba. la función debería devolver undefined.
+
+cls()
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      num = arr[i];
+      return num
+    }
+  }
+  return undefined;
+}
+
+//findElement([1, 2, 3, 4], num => num % 2 === 0);
+findElement([1, 3, 5, 8, 9, 10], function (num) { return num % 2 === 0; })
+
+//todo Boo who (Booleano Quién)
+//Comprueba si el valor está clasificado como booleano primitivo. Devuelve true o false. Booleanos primitivos son true y false.
+
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
+
+booWho(null);
+
+
+//todo Haz que la primera letra de una palabra este en mayúscula
+//Devuelve la cadena proporcionada con la primera letra de cada palabra en mayúsculas. Asegúrate de que el resto de la palabra esté en minúsculas. Como propósito de este ejercicio, debes también usar mayúsculas conectando palabras como the y of.
+
+function titleCase(str) {
+  str = str.split(' ');
+
+  for (let i = 0; i < str.length; i++) {
+    let word = str[i];
+    let firstWord = word.charAt(0).toUpperCase();
+    let restWord = word.slice(1).toLowerCase();
+    console.log(firstWord, restWord)
+    str[i] = firstWord + restWord;
+  }
+  return str.join(' ');
+}
+
+let test = titleCase("I'm a little tea pot");
+console.log(test);
